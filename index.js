031604beffc9fad9fbbdd03eb8a5ec7d11c44f93
trungitk16nbk <buiquoctrung2002@gmail.com>
2023-08-28 09:11:56 +0700

    Add domain allowedOrigins

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
-'use strict'; 
+'use strict';
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const cors = require('cors'); // Đảm bảo bạn đã cài đặt thư viện cors
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
+
 // Cấu hình tùy chỉnh
-app.options('/charity', cors()); // Xác định các tùy chọn cho Preflight request
-app.use((req, res, next) => {
-    const allowedOrigins = [
-        'https://trung-spice.myshopify.com',
-        'https://vnspice.myshopify.com'
-        // Thêm các tên miền khác nếu cần
-    ];
+app.use(cors({
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            'https://trung-spice.myshopify.com',
+            'https://vnspice.myshopify.com'
+            // Thêm các tên miền khác nếu cần
+        ];
 
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     }
-
-    // Các cài đặt CORS khác...
-    next();
-});
-
+}));
 
 app.get('/createTables', (req, res) => { 
     let models = require('./models');
     models.sequelize.sync().then(() => {
         res.send('tables created!')
-    })
-})
+    });
+});
 
 // routers
 app.get('/', (req, res) => {
     res.send('hello world');
-})
+});
 
 app.use('/charity', require('./routes/indexRouter'));
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
